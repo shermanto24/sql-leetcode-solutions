4611b346a3bd63f03f5263dde413a666f024ecbf
Stefani Hermanto <83617441+shermanto24@users.noreply.github.com>
2026-09-17 14:44:26 -0400

    Time: 1091 ms (19.70%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,23 +1,4 @@
 # Write your MySQL query statement below
--- select 
---     product_id,
---     round(sum(total_price) / sum(units), 2) as average_price
--- from
--- (
---     select
---         u.*,
---         units * p.price as total_price
---     from unitsSold as u
---         right join prices as p
---             on -- these two conditions don't work anymore
---                 u.product_id = p.product_id and 
---                 purchase_date between start_date and end_date
--- ) as merged_table
--- group by product_id
--- ;
--- 15/18 passed, what if unitssold is empty?
--- for one or all products
-
 select 
     product_id,
     round(coalesce(sum(total_price) / sum(units), 0), 2) as average_price
