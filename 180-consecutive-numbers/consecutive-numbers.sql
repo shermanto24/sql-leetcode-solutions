fbf046588d60fde2cc93d0eb1dfdfa433b3f0929
Stefani Hermanto <83617441+shermanto24@users.noreply.github.com>
2026-09-23 10:34:18 -0400

    Time: 699 ms (28.20%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -3,14 +3,14 @@ with consecutive_nums as
 (
     select
         *,
-        lead(num, 1) over (order by id asc) as one_down,
-        lead(num, 2) over (order by id asc) as two_down
+        lead(num, 1) over (order by id asc) as one_after,
+        lead(num, 2) over (order by id asc) as two_after
     from logs
 )
 select num as ConsecutiveNums
 from consecutive_nums
 where
-    one_down = num and 
-    two_down = num
+    one_after = num and 
+    two_after = num
 group by num
 ;
