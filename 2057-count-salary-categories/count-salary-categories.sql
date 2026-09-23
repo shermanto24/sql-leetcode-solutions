7c65ed64336d0c8a6c4ced5683a497af7c8198c5
Stefani Hermanto <83617441+shermanto24@users.noreply.github.com>
2026-09-23 15:31:09 -0400

    Time: 1529 ms (76.28%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,25 +1,39 @@
 # Write your MySQL query statement below
-with accounts_categorized as
-(
-    select
-        account_id,
-        case
-            when income < 20000 then 'Low Salary'
-            when income <= 50000 then 'Average Salary'
-            else 'High Salary'
-        end as category
-    from accounts
-)
-select 
-    c.category,
-    count(account_id) as accounts_count
-from accounts_categorized as a
-    right join
-    (
-        select 'Low Salary' as category union
-        select 'Average Salary' union
-        select 'High Salary'
-    ) as c
-        on c.category = a.category
-group by c.category
-;
+select 'High Salary' as category, count(account_id) as accounts_count
+from accounts where income > 50000
+
+union 
+
+select 'Low Salary' as category, count(account_id) as accounts_count
+from accounts where income < 20000
+
+union
+
+select 'Average Salary' as category, count(account_id) as accounts_count
+from accounts where income >= 20000 and income <= 50000
+
+-- Previous solution
+-- with accounts_categorized as
+-- (
+--     select
+--         account_id,
+--         case
+--             when income < 20000 then 'Low Salary'
+--             when income <= 50000 then 'Average Salary'
+--             else 'High Salary'
+--         end as category
+--     from accounts
+-- )
+-- select 
+--     c.category,
+--     count(account_id) as accounts_count
+-- from accounts_categorized as a
+--     right join
+--     (
+--         select 'Low Salary' as category union
+--         select 'Average Salary' union
+--         select 'High Salary'
+--     ) as c
+--         on c.category = a.category
+-- group by c.category
+-- ;
