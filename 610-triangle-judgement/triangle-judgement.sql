5b2cee94c4879a4252c5cf8e48fb698ef00ccef8
Stefani Hermanto <83617441+shermanto24@users.noreply.github.com>
2026-09-23 10:24:08 -0400

    Time: 382 ms (17.26%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -1,25 +1,36 @@
 -- # Write your MySQL query statement below
-with sides_and_index as 
-(
-    select
-        *,
-        case
-            when x > y and x > z then 1
-            when y > x and y > z then 2
-            else 3
-        end as longest_idx
-    from triangle
-)
 select
-    x, y, z,
+    *,
     if(
-        x <= 0 or y <= 0 or z <= 0,
-        'No',
-        case longest_idx
-            when 1 then if(y + z > x, 'Yes', 'No')
-            when 2 then if(x + z > y, 'Yes', 'No')
-            else if(x + y > z, 'Yes', 'No')
-        end
-    ) as triangle
-from sides_and_index
+        x + y > z and y + z > x and z + x > y,
+        'Yes', 'No'
+    )
+    as triangle
+from triangle
 ;
+
+-- Previous solution
+-- with sides_and_index as 
+-- (
+--     select
+--         *,
+--         case
+--             when x > y and x > z then 1
+--             when y > x and y > z then 2
+--             else 3
+--         end as longest_idx
+--     from triangle
+-- )
+-- select
+--     x, y, z,
+--     if(
+--         x <= 0 or y <= 0 or z <= 0,
+--         'No',
+--         case longest_idx
+--             when 1 then if(y + z > x, 'Yes', 'No')
+--             when 2 then if(x + z > y, 'Yes', 'No')
+--             else if(x + y > z, 'Yes', 'No')
+--         end
+--     ) as triangle
+-- from sides_and_index
+-- ;
