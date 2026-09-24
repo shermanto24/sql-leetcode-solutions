21f178629cde0b314bfe040e8bf564bf954682ec
Stefani Hermanto <83617441+shermanto24@users.noreply.github.com>
2026-09-24 12:26:05 -0400

    Time: 597 ms (95.92%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,8 +1,22 @@
 # Write your MySQL query statement below
-delete p1
-from person as p1
-    inner join person as p2
-        on
-            p1.email = p2.email and
-            p2.id < p1.id
+delete from person
+where id not in -- deleting ids that are not the minimum
+(
+    select id
+    from
+    (   -- gets 1 id per email, id is the min one associated
+        select min(id) as id
+        from person
+        group by email
+    ) as t
+)
 ;
+
+-- Previous solution
+-- delete p1 -- looks at result table from rest of code, deletes that from person
+-- from person as p1
+--     inner join person as p2
+--         on
+--             p1.email = p2.email and
+--             p2.id < p1.id
+-- ;
